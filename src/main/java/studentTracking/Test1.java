06872c0fcf5f26f0866ed8ae861e51dfc56e260e
admin <1126107509@qq.com>
2020-09-21 16:56:11 +0800

    添加修改信息

---
+++ src/main/java/studentTracking/Test1.java
@@ -0,0 +1,19 @@
+/**
+ * @description Todo
+ * @author li
+ * @date 2020-09-21 15:43
+ */
+package studentTracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import studentTracking.model.Menu;
+import studentTracking.service.IMenuService;
+
+import java.util.List;
+
+public class Test1 {
+
+
+
+}
